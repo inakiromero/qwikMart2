7ca575f52461be7d7248d39f57ba5107f2e5b48a
inakiromero <tatijavier_romero@hotmail.com>
2024-11-26 18:53:03 -0300

    utilizacion de los diseños en el proyecto principal

--- src/app/usuarios/registro/registro.component.ts
+++ src/app/usuarios/registro/registro.component.ts
@@ -5,6 +5,7 @@ import { Usuario } from '../usuario.model';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent {
   usuario: Usuario = {
